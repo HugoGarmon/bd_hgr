940a14a460e61790c472d79c7bfd2b84b6e43f50
Hugo Garmon <Hugo@Garmon.com>
2026-01-14 18:26:23 +0100

    24

--- Big_Data_Aplicado/hadoop/notebooks/pro403/mapper1.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-import sys 
-
-for line in sys.stdin:
-    line = line.strip().split()
-    print(f"{line[8]} {1}")
--- Big_Data_Aplicado/hadoop/notebooks/pro403/mapper3.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-import sys
-
-for line in sys.stdin:
-
-    line = line.strip().split()
-    print(f"{line[11]}, 1")
